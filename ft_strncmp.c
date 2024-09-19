6bd2b397655223a6c222adcc049eb9d16746edd5
Izhan Sastre Hernando <izhansastre@gmail.com>
2024-09-19 04:23:20 +0200

    norminetted ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,15 +6,18 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 04:04:19 by isastre-          #+#    #+#             */
-/*   Updated: 2024/09/19 04:18:19 by isastre-         ###   ########.fr       */
+/*   Updated: 2024/09/19 04:22:27 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	while (*s1 == *s2 && *s1 != '\0' && --n)
-		s1++, s2++;	
+	{
+		s1++;
+		s2++;
+	}
 	return (*s1 - *s2);
 }
