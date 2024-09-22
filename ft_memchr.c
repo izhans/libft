f5db1f01da6d02228cb4de1a2264b6c312b5d964
Izhan Sastre Hernando <izhansastre@gmail.com>
2024-09-23 00:49:12 +0200

    ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 16:04:34 by isastre-          #+#    #+#             */
+/*   Updated: 2024/09/23 00:42:58 by isastre-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*str;
+	size_t			i;
+
+	str = (unsigned char *) s;
+	i = 0;
+	while (i < n)
+	{
+		if (c == str[i])
+			return (str + i);
+		i++;
+	}
+	return (NULL);
+}
