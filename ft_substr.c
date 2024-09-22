9e56b70a27467d63cba9422227dc60a2869a6154
izhans <izhansastre@gmail.com>
2024-09-22 23:28:38 +0200

    corrected ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 16:32:52 by isastre-          #+#    #+#             */
-/*   Updated: 2024/09/19 21:20:33 by isastre-         ###   ########.fr       */
+/*   Updated: 2024/09/21 14:24:59 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	size;
 	size_t	i;
 
-	size = len + 1;
+	size = ft_strlen(s + start) + 1;
+	if (size > len)
+		size = len + 1;
 	substr = malloc(size * sizeof(char));
 	if (substr == NULL)
 		return (NULL);
 	i = 0;
-	while (s[start] && i < size)
+	while (s[start] && i < size - 1)
 	{
 		substr[i] = s[start];
 		i++;
 		start++;
 	}
+	substr[i] = '\0';
 	return (substr);
 }
--- tests/ft_substr_test.c
+++ tests/ft_substr_test.c
@@ -1,37 +1,59 @@
 #include "../libft.h"
 #include "_test_functions.c"
 #include <string.h>
+#include <stdlib.h>
 
 #define FUNCTION "substr"
 
-void test_substr()
+void test_substr_is_at_the_end()
 {
 	char *str = "No es cruel la vida";
-	int len = strlen(str) - 3;
-	char *substr = ft_substr(str, 3, len);
+	int start = 3;
+	int len = strlen(str) - start;
+	char *substr = ft_substr(str, start, len);
 
-	if(strcmp(str + 3, substr))
+	if(strcmp(str + start, substr))
 	{
 		print_test_fail(1, FUNCTION);
-		printf("Expected: >>>%s<<<\nActual: >>>%s<<<\n", str + 3, substr);
+		printf("Expected: >>>%s<<<\nActual: >>>%s<<<\n", str + start, substr);
 	}
+	free(substr);
 }
 
 void test_size_is_bigger_than_substr()
 {
+	// remember to check that malloc only allocates the memory we are really using
 	char *str = "p*ta vida";
+	int start = 5;
 	int len = strlen(str);
-	char *substr = ft_substr(str, 5, len);
+	char *substr = ft_substr(str, start, len);
 
-	if(strcmp(str + 5, substr))
+	if(strcmp(str + start, substr))
 	{
 		print_test_fail(2, FUNCTION);
-		printf("Expected: >>>%s<<<\nActual: >>>%s<<<\n", str + 5, substr);
+		printf("Expected: >>>%s<<<\nActual: >>>%s<<<\n", str + start, substr);
 	}
+	free(substr);
+}
+
+void test_substr_is_in_the_middle()
+{
+	char *str = "que lindo dia para comprobar que una funcion no rompe";
+	int start = 4;
+	int len = 9;
+	char *substr = ft_substr(str, start, len);
+	char *expected = "lindo dia";
+	if(strcmp(expected, substr))
+	{
+		print_test_fail(3, FUNCTION);
+		printf("Expected: >>>%s<<<\nActual: >>>%s<<<\n", expected, substr);
+	}
+	free(substr);
 }
 
 int main()
 {
-	test_substr();
+	test_substr_is_at_the_end();
 	test_size_is_bigger_than_substr();
+	test_substr_is_in_the_middle();
 }
