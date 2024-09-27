40d5864814009b23a96d4aa3d1c2a39e23d6a65b
izhans <izhansastre@gmail.com>
2024-09-27 23:26:46 +0200

    ft_strnstr: function wasnt looking correctly for the substr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 00:06:39 by isastre-          #+#    #+#             */
-/*   Updated: 2024/09/23 02:08:17 by isastre-         ###   ########.fr       */
+/*   Updated: 2024/09/27 23:22:51 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,18 @@
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
-	size_t	j;
 	size_t	little_len;
 
 	little_len = ft_strlen(little);
 	if (little_len == 0)
 		return ((char *) big);
-	if (little_len > len || little_len > ft_strlen(big))
-		return (NULL);
 	i = 0;
-	j = 0;
-	while (big[i] && i < len)
+	while (*big && i + little_len <= len)
 	{
-		if (big[i] == little[j])
-			j++;
-		else
-			j = 0;
-		if (j == little_len)
-			return ((char *) &big[i - j + 1]);
+		if (ft_strncmp(big, little, little_len) == 0)
+			return ((char *) big);
 		i++;
+		big++;
 	}
 	return (NULL);
 }
