2708eff75c70d01f6bdf52504a46b23b2cf19bff
izhans <izhansastre@gmail.com>
2024-09-28 13:34:42 +0200

    ft_memcpy: fixed crash when dest and src where NULL

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 20:20:49 by isastre-          #+#    #+#             */
-/*   Updated: 2024/09/18 21:15:40 by isastre-         ###   ########.fr       */
+/*   Updated: 2024/09/28 13:23:55 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	char	*s;
 	size_t	i;
 
+	if (dest == NULL && src == NULL)
+		return (dest);
 	s = (char *) src;
 	d = (char *) dest;
 	i = 0;
