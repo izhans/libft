5a0663b741c9f9d143b4e2ec8f4f919f5f9911d7
izhans <izhansastre@gmail.com>
2024-09-28 13:04:02 +0200

    ft_memchr: fixed char to be found wasn't being transformed to an unsigned char (overflows failed)

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 16:04:34 by isastre-          #+#    #+#             */
-/*   Updated: 2024/09/23 00:42:58 by isastre-         ###   ########.fr       */
+/*   Updated: 2024/09/28 13:02:19 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*str;
+	unsigned char	ch;
 	size_t			i;
 
 	str = (unsigned char *) s;
+	ch = c;
 	i = 0;
 	while (i < n)
 	{
-		if (c == str[i])
+		if (ch == str[i])
 			return (str + i);
 		i++;
 	}
