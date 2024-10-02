492dc35a06a15cb0d683a5425448c585b268edc5
izhans <izhansastre@gmail.com>
2024-10-02 19:30:19 +0200

    ft_substr: enhaced some ifs

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 16:32:52 by isastre-          #+#    #+#             */
-/*   Updated: 2024/10/02 18:37:53 by isastre-         ###   ########.fr       */
+/*   Updated: 2024/10/02 19:29:30 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	str_len = ft_strlen(s);
 	if (start >= str_len)
+	{
 		len = 0;
-	if (len > str_len + start)
+		start = 0;
+	}
+	else if (len > str_len + start)
 		len = str_len - start;
 	substr = malloc((len + 1) * sizeof(char));
 	if (substr == NULL)
 		return (NULL);
-	if (len == 0)
-		substr[0] = '\0';
-	else
-		ft_strlcpy(substr, s + start, len + 1);
+	ft_strlcpy(substr, s + start, len + 1);
 	return (substr);
 }
