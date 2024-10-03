bbbe1c731f62de1f9e50fcdcefdd7a11ddd04b85
Izhan Sastre Hernando <izhansastre@gmail.com>
2024-10-03 17:02:31 +0200

    wip: ft_split - arraso con todo y volvemos a empezar poquito a poco

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 04:35:47 by isastre-          #+#    #+#             */
-/*   Updated: 2024/10/03 16:57:34 by isastre-         ###   ########.fr       */
+/*   Updated: 2024/10/03 17:01:35 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,76 +18,5 @@ void	ft_free(char	**strs, size_t i);
 #include <stdio.h>
 char **ft_split(char const *s, char c)
 {
-	size_t	n_parts;
-	char	*cleaned_str;
-	char	**strs;
-
-	cleaned_str = ft_strtrim(s, &c);
-	n_parts = ft_count_number_of_parts(cleaned_str, c);
-	strs = malloc((n_parts + 1) * sizeof(char *));
-	if (strs == NULL)
-		return (NULL);
 	
-	size_t	i;
-	size_t	len;
-
-	i = 0;
-	while (i < n_parts && ft_strchr(s, c))
-	{
-		printf("part %zu\n", i);
-		len = ft_strchr(s, c) - s;
-		printf("\tlen: %zu\n", len);
-		strs[i] = ft_substr(s, 0, len);
-		printf("\tstr[%zu]: %s\n", i, strs[i]);
-		if (strs[i] == NULL)
-		{
-			printf("free");
-			ft_free(strs, i);
-			return (NULL);
-		}
-		i++;
-		printf("\t\tstrchr: <%s>\n", ft_strchr(s + 1, c) + 1);
-		s = ft_strchr(s + 1, c) + 1;
-		printf("\tnew s: <%s>\n", s);
-		while (*s == c)
-		{
-			printf("aaa\n");
-			printf("\tnew s: <%s>\n", ft_strchr(s + 1, c));
-			s = ft_strchr(s+1, c);
-
-		}
-		
-		
-	}
-	strs[i] = NULL;
-	return (strs);
-}
-
-size_t	ft_count_number_of_parts(char const *str, char c)
-{
-	size_t	n_parts;
-
-	n_parts = 1;
-	// printf("str: <%s>\n", str);
-	while (*str && ft_strchr(str + 1, c))
-	{
-		str = ft_strchr(str + 1, c) + 1;
-		// printf("\t%zu <%s>\n", n_parts, str);
-		if (*str != c)
-			n_parts++;
-	}
-	printf("nparts: %zu\n", n_parts);
-	return (n_parts);
-}
-
-// libero todo lo que se había reservado hasta el momento
-// TODO probar a que la funcion devuelva NULL y arriba hacer return de la funcion directamente
-void	ft_free(char	**strs, size_t i)
-{
-	while(i > 0)
-	{
-		i--;
-		free(strs[i]);
-	}
-	free(strs);
 }
--- tests/ft_split_test.c
+++ tests/ft_split_test.c
@@ -4,63 +4,8 @@
 
 #define FUNCTION "split"
 
-void test1()
-{
-	char *s = "";
-	char c = 'a';
-
-	char *expected[] = {""};
-	char **actual = ft_split(s, c);
-
-	int i = 0;
-	while (expected[i])
-	{
-		printf("expected[%d]: %s actual[%d]: %s\n", i, expected[i], i, actual[i]);
-		if (strcmp(expected[i], actual[i]))
-			print_test_fail(1, FUNCTION);
-	}
-}
-void test2()
-{
-	char *s = "hola mundo";
-	char c = 0;
-
-	char *expected[] = {"hola mundo"};
-	char **actual = ft_split(s, c);
-
-	int i = 0;
-	while (expected[i])
-	{
-		printf("expected[%d]: %s actual[%d]: %s\n", i, expected[i], i, actual[i]);
-		if (strcmp(expected[i], actual[i]))
-			print_test_fail(1, FUNCTION);
-	}
-}
-void test3()
-{
-	char *s = "hola mundo";
-	char c = ' ';
-
-	char *expected[] = {"hola", "mundo"};
-	char **actual = ft_split(s, c);
-
-	int i = 0;
-	while (expected[i])
-	{
-		printf("expected[%d]: %s actual[%d]: %s\n", i, expected[i], i, actual[i]);
-		if (strcmp(expected[i], actual[i]))
-			print_test_fail(1, FUNCTION);
-	}
-}
-
-
-
 int main()
 {
-	// test1();
-	// test2();
-	// test3();
-
 	char *str = "Hola mundo";
 	char c = ' ';
 
@@ -77,7 +22,6 @@ int main()
 		printf("<%s>\n", *res);
 		res++;
 	}
-	// free(res);
 	
 	return 0;
 }
