6140c0f174557959013042e2e35debf94cc44303
Izhan Sastre Hernando <izhansastre@gmail.com>
2025-03-21 15:12:17 +0100

    reorganized lib into folders

---
+++ parse/ft_atoi.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 03:16:18 by isastre-          #+#    #+#             */
+/*   Updated: 2025/03/21 15:03:37 by isastre-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+static int	ft_isspace(int c);
+
+int	ft_atoi(const char *nptr)
+{
+	int		n;
+	int		sign;
+	char	*str;
+
+	n = 0;
+	sign = 1;
+	str = (char *) nptr;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (ft_isdigit(*str))
+	{
+		n = n * 10 + (*str - '0');
+		str++;
+	}
+	return (n * sign);
+}
+
+static int	ft_isspace(int c)
+{
+	return ((c >= 9 && c <= 13) || c == ' ');
+}
